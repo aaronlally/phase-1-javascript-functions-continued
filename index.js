d399d6bf3499f0dab1a1018ad24ed03733c78f0b
Aaron Lally <aaronblally@gmail.com>
2022-04-28 17:08:26 -0400

    commit tree e8e057ad02b526f1f6af9b03678ec250890f3910

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const saturdayFun = (thing = "roller-skate") => {
+    return `This Saturday, I want to ${thing}!`;
+}
+
+const mondayWork = (stuff = "go to the office") => {
+    return `This Monday, I will ${stuff}.`;
+}
+
+const wrapAdjective = (thing = "*") => {
+    function first(stuff = "special") {
+        return `You are ${thing}${stuff}${thing}!`
+    }
+    return first
+}
+wrapAdjective("%")("a dedicated programmer");
